6068d9335159a0d650da664a9aaad8ae22bd7762
Krakaw <41575888+Krakaw@users.noreply.github.com>
2021-07-18 11:59:13 +0200

    Added better errors and move destination

--- src/error.rs
+++ src/error.rs
@@ -4,8 +4,13 @@ use thiserror::Error;
 pub enum RrenamerError {
     #[error("Movie Not Found")]
     MovieNotFound,
-    #[error("Invalid filename")]
-    InvalidFilename,
+    #[error("Invalid filename: {0}")]
+    InvalidFilename(String),
+    #[error("Invalid file extension: {0}")]
+    InvalidFileExt(String),
+    #[error("Invalid path: {0}")]
+    InvalidPath(String),
+
     #[error(transparent)]
     ApiError(#[from] reqwest::Error),
     #[error(transparent)]
--- src/files/input_file.rs
+++ src/files/input_file.rs
@@ -1,17 +1,19 @@
 use std::path::{Path, PathBuf};
 use crate::error::RrenamerError;
-use crate::error::RrenamerError::{InvalidFilename, MovieNotFound};
+use crate::error::RrenamerError::{InvalidFilename, MovieNotFound, InvalidFileExt, InvalidPath};
 
 pub struct InputFile {
     pub input_path: PathBuf,
     pub movie_name: Option<String>,
+    pub output_dir: Option<PathBuf>,
 }
 
 impl InputFile {
-    pub fn new (path: &str)-> Result<InputFile, RrenamerError> {
+    pub fn new(path: &str, output_dir: Option<PathBuf>) -> Result<InputFile, RrenamerError> {
         Ok(InputFile {
             input_path: Path::new(path).to_path_buf(),
-            movie_name: None
+            movie_name: None,
+            output_dir,
         })
     }
 
@@ -20,25 +22,27 @@ impl InputFile {
     }
 
     pub fn lookup_parts(&self) -> Result<(String, String, String), RrenamerError> {
-        let file_name = self.input_path.file_stem().ok_or(InvalidFilename)?.to_string_lossy();
-        let ext = self.input_path.extension().ok_or(InvalidFilename)?.to_string_lossy();
+        let file_name = self.input_path.file_stem().ok_or(InvalidFilename("".to_string()))?.to_string_lossy();
+        let ext = self.input_path.extension().ok_or(InvalidFilename("".to_string()))?.to_string_lossy();
         let re = regex::Regex::new(r"^(.*?)+\.?(\d{4})+.*?\.?([a-zA-Z0-9]{3,4})?$").unwrap();
         let caps = re.captures(&file_name);
 
         let search;
         let year;
         if let Some(cap) = caps {
-            search = format!("{}", cap.get(1).map(|m|m.as_str()).unwrap()).replace('.', " ");
-            year = format!("{}", cap.get(2).map(|m|m.as_str()).unwrap()).replace('.', " ");
+            search = format!("{}", cap.get(1).map(|m| m.as_str()).unwrap()).replace('.', " ");
+            year = format!("{}", cap.get(2).map(|m| m.as_str()).unwrap()).replace('.', " ");
         } else {
-            return Err(InvalidFilename);
+            return Err(InvalidFilename (file_name.to_string()));
         }
         Ok((search, year, ext.into()))
     }
 
     pub fn output_path(&self) -> Result<PathBuf, RrenamerError> {
-        let ext = self.input_path.extension().ok_or(InvalidFilename)?.to_string_lossy();
-        let output = self.input_path.parent().ok_or(InvalidFilename)?.join(format!("{}.{}", self.movie_name.as_ref().ok_or(MovieNotFound)?, ext));
+        let ext = self.input_path.extension().ok_or(InvalidFileExt("".to_string()))?.to_string_lossy();
+        let input_parent = &self.input_path.parent().ok_or(InvalidPath("".to_string()))?.to_path_buf();
+        let parent = self.output_dir.as_ref().unwrap_or_else(|| input_parent);
+        let output = parent.join(format!("{}.{}", self.movie_name.as_ref().ok_or(MovieNotFound)?, ext));
         Ok(output)
     }
 
@@ -47,8 +51,4 @@ impl InputFile {
         std::fs::rename(self.input_path, output_path.clone())?;
         Ok(output_path)
     }
-
-
-
-
 }
