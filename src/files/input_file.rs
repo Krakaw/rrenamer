e7f922d4096f43aa19048b2a28c58450ba4a8cff
Krakaw <41575888+Krakaw@users.noreply.github.com>
2021-07-18 19:34:26 +0200

    Fixing merge

--- src/files/input_file.rs
+++ src/files/input_file.rs
@@ -1,89 +1,56 @@
-use std::path::{Path, PathBuf};
 use crate::error::RrenamerError;
-use crate::error::RrenamerError::{InvalidFilename, MovieNotFound, InvalidFileExt, InvalidPath};
-
+use crate::error::RrenamerError::{InvalidFileExt, InvalidFilename, InvalidPath, MovieNotFound};
+use crate::files::file_parts::FileParts;
+use std::convert::TryFrom;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
 pub struct InputFile {
-    pub input_path: PathBuf,
-    pub movie_name: Option<String>,
+    pub input_file_parts: FileParts,
     pub output_dir: Option<PathBuf>,
-}
 
-#[derive(Debug, Copy, Clone)]
-pub enum FileTypes {
-    Movie,
-    Series,
-}
+    pub input_path: PathBuf,
 
-impl Default for FileTypes {
-    fn default() -> Self {
-        Self::Movie
-    }
-}
-
-#[derive(Default, Debug, Clone)]
-pub struct FileParts {
-    pub file_type: FileTypes,
-    pub name: String,
-    pub year: Option<String>,
-    pub season: Option<String>,
-    pub episode: Option<String>,
-    pub title: Option<String>,
-    pub ext: String,
+    pub output_file_name: Option<String>,
 }
 
 impl InputFile {
     pub fn new(path: &str, output_dir: Option<PathBuf>) -> Result<InputFile, RrenamerError> {
-        Ok(InputFile {
-            input_path: Path::new(path).to_path_buf(),
-            movie_name: None,
+        let input_file_parts = FileParts::try_from(path)?;
+        if let Some(output_dir) = output_dir.as_ref() {
+            if !output_dir.exists() {
+                return Err(InvalidPath(output_dir.to_string_lossy().to_string()));
+            }
+        }
+        let input_file = InputFile {
+            input_file_parts,
             output_dir,
-        })
+
+            input_path: Path::new(path).to_path_buf(),
+            output_file_name: None,
+        };
+        Ok(input_file)
     }
 
-    pub fn set_movie_name(&mut self, movie_name: String) {
-        self.movie_name = Some(movie_name);
-    }
-
-    pub fn lookup_parts(&self) -> Result<FileParts, RrenamerError> {
-        let file_name = self.input_path.file_stem().ok_or(InvalidFilename)?.to_string_lossy();
-        let ext = self.input_path.extension().ok_or(InvalidFilename)?.to_string_lossy();
-        let movie_regex = regex::Regex::new(r"^(.*?)+\.?(\d{4})+.*?\.?([a-zA-Z0-9]{3,4})?$").unwrap();
-        let series_regex = regex::Regex::new(r"^(.*?)+\.?([sS]+(\d+)[eE]+(\d+))+.*?\.?([a-zA-Z0-9]{3,4})?$").unwrap();
-
-
-        if let Some(cap) = movie_regex.captures(&file_name) {
-            let name = format!("{}", cap.get(1).map(|m| m.as_str()).unwrap()).replace('.', " ");
-            let year = format!("{}", cap.get(2).map(|m| m.as_str()).unwrap()).replace('.', " ");
-            return Ok(FileParts {
-                name,
-                file_type: FileTypes::Movie,
-                year: Some(year),
-                ext: ext.to_string(),
-                ..Default::default()
-            });
+    pub fn set_output_file_name(self, output_file_name: String) -> Self {
+        InputFile {
+            output_file_name: Some(output_file_name),
+            ..self
         }
-        if let Some(cap) = series_regex.captures(&file_name) {
-            eprintln!("cap = {:?}", cap);
-            let name = cap.get(1).and_then(|m| Some(m.as_str())).ok_or(InvalidFilename)?.to_string();
-            let episode = cap.get(3).and_then(|m| Some(m.as_str())).ok_or(InvalidFilename)?.to_string();
-            let season = cap.get(4).and_then(|m| Some(m.as_str())).ok_or(InvalidFilename)?.to_string();
-            return Ok(FileParts {
-                name,
-                episode: Some(episode),
-                season: Some(season),
-                file_type: FileTypes::Series,
-                ..Default::default()
-            });
-        }
-
-        return Err(InvalidFilename);
     }
 
     pub fn output_path(&self) -> Result<PathBuf, RrenamerError> {
-        let ext = self.input_path.extension().ok_or(InvalidFileExt("".to_string()))?.to_string_lossy();
-        let input_parent = &self.input_path.parent().ok_or(InvalidPath("".to_string()))?.to_path_buf();
+        let ext = &self.input_file_parts.ext;
+        let input_parent = &self
+            .input_path
+            .parent()
+            .ok_or(InvalidPath("".to_string()))?
+            .to_path_buf();
         let parent = self.output_dir.as_ref().unwrap_or_else(|| input_parent);
-        let output = parent.join(format!("{}.{}", self.movie_name.as_ref().ok_or(MovieNotFound)?, ext));
+        let output = parent.join(format!(
+            "{}.{}",
+            self.output_file_name.as_ref().ok_or(MovieNotFound)?,
+            ext
+        ));
         Ok(output)
     }
 
--- src/lookup/tmdb.rs
+++ src/lookup/tmdb.rs
@@ -1,103 +1,124 @@
 use crate::error::RrenamerError;
 use crate::error::RrenamerError::MovieNotFound;
-use crate::lookup::tmdb_results::{TmdbResults, TmdbResult};
-use crate::files::input_file::{FileParts, FileTypes};
-
+use crate::files::file_parts::{FileParts, FileTypes};
+use crate::lookup::tmdb_results::{TmdbResult, TmdbResults};
+use std::marker::PhantomData;
 
 pub struct Tmdb;
 
-
 impl Tmdb {
-    pub async fn lookup(file_parts: FileParts) -> Result<TmdbResults, RrenamerError> {
+    pub async fn lookup(file_parts: &FileParts) -> Result<TmdbResults, RrenamerError> {
         match file_parts.file_type {
-            FileTypes::Movie => {
-                Tmdb::lookup_movie(file_parts).await
-            }
-            FileTypes::Series => {
-                Tmdb::lookup_series(file_parts).await
-            }
+            FileTypes::Movie => Tmdb::lookup_movie(file_parts).await,
+            FileTypes::Series => Tmdb::lookup_series(file_parts).await,
         }
     }
 
-    pub async fn lookup_series(file_parts: FileParts) -> Result<TmdbResults, RrenamerError> {
-        let search = file_parts.name;
-        let season = file_parts.season.unwrap_or("".to_string());
-        let episode = file_parts.episode.unwrap_or("".to_string());
+    pub async fn lookup_series(file_parts: &FileParts) -> Result<TmdbResults, RrenamerError> {
+        let search = file_parts.name.clone();
+        let season = file_parts
+            .season
+            .as_ref()
+            .unwrap_or(&"".to_string())
+            .clone();
+        let episode = file_parts
+            .episode
+            .as_ref()
+            .unwrap_or(&"".to_string())
+            .clone();
 
         let api_key = dotenv::var("API_KEY").unwrap();
         let r = reqwest::get(format!(
             "https://api.themoviedb.org/3/search/tv?api_key={}&query={}",
-            api_key,
-            search,
+            api_key, search,
         ))
-            .await?
-            .json::<serde_json::Value>()
-            .await?;
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
 
         eprintln!("r = {:?}", r);
-        let results = r.get("results").ok_or(MovieNotFound)?.as_array().ok_or(MovieNotFound)?.iter().map(|m| {
-
-            let title = m.get("original_title").map(|v| {
-                if v.is_string() {
-                    v.as_str().unwrap()
-                } else {
-                    ""
-                }
-            }).unwrap_or("").to_string();
-            let year = m.get("release_date").map(|v| {
-                if v.is_string() {
-                    v.as_str().unwrap().split('-').next().unwrap_or("")
-                } else {
-                    ""
-                }
-            }).unwrap_or("").to_string();
-            TmdbResult {
-                title,
-                year,
-            }
-        }).collect::<_>();
+        let results = r
+            .get("results")
+            .ok_or(MovieNotFound)?
+            .as_array()
+            .ok_or(MovieNotFound)?
+            .iter()
+            .map(|m| {
+                let title = m
+                    .get("original_title")
+                    .map(|v| {
+                        if v.is_string() {
+                            v.as_str().unwrap()
+                        } else {
+                            ""
+                        }
+                    })
+                    .unwrap_or("")
+                    .to_string();
+                let year = m
+                    .get("release_date")
+                    .map(|v| {
+                        if v.is_string() {
+                            v.as_str().unwrap().split('-').next().unwrap_or("")
+                        } else {
+                            ""
+                        }
+                    })
+                    .unwrap_or("")
+                    .to_string();
+                TmdbResult { title, year }
+            })
+            .collect::<_>();
 
         Ok(TmdbResults(results))
     }
 
-    pub async fn lookup_movie(file_parts: FileParts) -> Result<TmdbResults, RrenamerError> {
-        let search = file_parts.name;
-        let year = file_parts.year.unwrap_or("".to_string());
+    pub async fn lookup_movie(file_parts: &FileParts) -> Result<TmdbResults, RrenamerError> {
+        let search = file_parts.name.clone();
+        let year = file_parts.year.as_ref().unwrap_or(&"".to_string()).clone();
 
         let api_key = dotenv::var("API_KEY").unwrap();
         let r = reqwest::get(format!(
             "https://api.themoviedb.org/3/search/movie?api_key={}&query={}&year={}",
-            api_key,
-            search,
-            year
+            api_key, search, year
         ))
-            .await?
-            .json::<serde_json::Value>()
-            .await?;
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
 
-        let results = r.get("results").ok_or(MovieNotFound)?.as_array().ok_or(MovieNotFound)?.iter().map(|m| {
-            let title = m.get("original_title").map(|v| {
-                if v.is_string() {
-                    v.as_str().unwrap()
-                } else {
-                    ""
-                }
-            }).unwrap_or("").to_string();
-            let year = m.get("release_date").map(|v| {
-                if v.is_string() {
-                    v.as_str().unwrap().split('-').next().unwrap_or("")
-                } else {
-                    ""
-                }
-            }).unwrap_or("").to_string();
-            TmdbResult {
-                title,
-                year,
-            }
-        }).collect::<_>();
+        let results = r
+            .get("results")
+            .ok_or(MovieNotFound)?
+            .as_array()
+            .ok_or(MovieNotFound)?
+            .iter()
+            .map(|m| {
+                let title = m
+                    .get("original_title")
+                    .map(|v| {
+                        if v.is_string() {
+                            v.as_str().unwrap()
+                        } else {
+                            ""
+                        }
+                    })
+                    .unwrap_or("")
+                    .to_string();
+                let year = m
+                    .get("release_date")
+                    .map(|v| {
+                        if v.is_string() {
+                            v.as_str().unwrap().split('-').next().unwrap_or("")
+                        } else {
+                            ""
+                        }
+                    })
+                    .unwrap_or("")
+                    .to_string();
+                TmdbResult { title, year }
+            })
+            .collect::<_>();
 
         Ok(TmdbResults(results))
     }
-
-
 }
